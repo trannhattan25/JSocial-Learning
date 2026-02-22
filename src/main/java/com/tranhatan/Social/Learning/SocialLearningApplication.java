8c22c21ea148346ac396fce31575677b02602b7e
trannhattan25 <tanwaifu125@gmail.com>
2026-02-22 22:37:39 +0700

    Set up project:folder & config

---
+++ src/main/java/com/tranhatan/Social/Learning/SocialLearningApplication.java
@@ -0,0 +1,13 @@
+package com.tranhatan.Social.Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tranhatan/Social/Learning/SocialLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranhatan.Social.Learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
